5f8eef0959a1a9878950582911c0b77216169655
Heberti Almeida <hebertialmeida@gmail.com>
2014-08-27 15:24:24 -0300

    Initial implementation

---
+++ CocoaFontsDemo/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  CocoaFonts
+//
+//  Created by Heberti Almeida on 27/08/14.
+//  Copyright (c) 2014 CocoaFonts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let label = UILabel()
+        label.text = "Font test"
+        label.frame = CGRectMake(0, 0, self.view.frame.width, 100)
+        label.center = self.view.center
+        label.textAlignment = NSTextAlignment.Center
+        label.font = Fonts().latoHairlineOfSize(70)
+        self.view.addSubview(label)
+        
+//        Fonts.printFamilyNames()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
